30d1ee7147c2e57701a45857d71917328ae08aae
Lucilus <lucas.mage@proton.me>
2026-07-30 00:18:50 +0200

    chore: port reponseUtils to TS

--- src/services/responseUtils.ts
+++ src/services/responseUtils.ts
@@ -1,6 +1,6 @@
 import { splitCookiesString } from "set-cookie-parser";
 
-export const getCookiesFromResponse = (response) => {
+export const getCookiesFromResponse = (response: Response): string | string[] => {
     const setCookieHeader = getHeaderFromResponse({ response, item: "set-cookie" });
     if (setCookieHeader === null) return [];
 
@@ -9,30 +9,39 @@ export const getCookiesFromResponse = (response) => {
     )[0];
 };
 
-export const getHeaderFromResponse = ({ response, item }) => {
+export const getHeaderFromResponse = ({
+    response,
+    item,
+}: {
+    response: Response;
+    item: string;
+}): string | null => {
     const headers = response.headers;
 
     return isHeaderInstance(headers) ? headers.get(item) : item;
 };
 
-const isHeaderInstance = (headers) => typeof headers.get === "function";
+const isHeaderInstance = (headers: any): boolean =>
+    typeof headers?.get === "function";
 
-export const convertApiResponse = async (response) => {
+export const convertApiResponse = async (response: Response): Promise<any> => {
     const stringifyResponse = await response.text();
 
     if (stringifyResponse) {
         try {
             return JSON.parse(stringifyResponse);
         } catch (error) {
-            console.warn("Échec du parsing JSON (panne ou maintenance d'EcoleDirecte ?) :", error);
+            console.warn(
+                "Échec du parsing JSON (panne ou maintenance d'EcoleDirecte ?) :",
+                error
+            );
             return {
                 code: 502,
                 message: "Serveur EcoleDirecte indisponible ou en maintenance.",
-                data: {}
+                data: {},
             };
         }
     } else {
         return response;
     }
 };
-
