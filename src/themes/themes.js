38253fcaffef97be1eb07ec7db61538ad696d9ee
Lucilus <lucas.mage@proton.me>
2026-08-21 18:39:02 +0200

    feat: add new colors norms

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -66,7 +66,7 @@ export const THEMES = {
             ...NavigationDefaultTheme.colors,
             // background: "rgb(18, 18, 138)", // default key
             background: {
-                gradient: ["rgb(35, 20, 130)", "rgb(10, 10, 30)", 0.28], // +20 or +30
+                gradient: ["rgb(35, 20, 130)", "rgb(10, 10, 30)", 0.28], //rgb(10, 10, 30)
                 login: "rgb(19, 19, 34)",
             },
             bg: {
@@ -93,7 +93,7 @@ export const THEMES = {
             error: "rgb(240, 90, 90)",
             fond: "#0E101A",
             pastel: "rgb(21, 25, 69)",
-            secondary: "rgb(33, 27, 70)",
+            secondary: "rgb(31, 30, 60)",
             main: "rgba(166, 166, 240, 1)",
             accent: "rgb(197, 197, 247)",
             case: "rgb(94, 94, 136)",
