bb60fccd9ab248c89563f5521f466e833d4277fe
Lucilus <lucas.mage@proton.me>
2026-08-25 00:24:03 +0200

    style: update homeworkdaterow

--- src/features/homeworks/components/HomeworkDatesRow.jsx
+++ src/features/homeworks/components/HomeworkDatesRow.jsx
@@ -1,57 +1,56 @@
 import { memo, useCallback } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
+import { useTheme } from "@/hooks/useThemeStore";
 
 import { Text } from "@/components/core";
 import { adjustLightness } from "@/utils/colorGenerator";
 
 export const HomeworkDateItem = memo(
     ({ contracted, isEvaluation, isActive, allTasksCompleted, onPress }) => {
-        let dateBackgroundColor;
-        if (isEvaluation && allTasksCompleted) {
-            dateBackgroundColor = "hsl(28, 48%, 33%)"; //temp color
-        } else if (isEvaluation) {
-            dateBackgroundColor = "hsl(2, 63%, 43%)"; // temp color
-        } else if (allTasksCompleted) {
-            dateBackgroundColor = "hsl(115, 33%, 38%)"; // maybe good ?
-        } else {
-            dateBackgroundColor = "hsl(240, 19%, 36%)"; // keep this
-        }
+        const { colors } = useTheme();
         return (
             <TouchableOpacity
                 style={{
-                    width: 58,
-                    height: 58,
+                    width: 65,
+                    height: isActive ? 90 : 65,
                     alignItems: "center",
                     justifyContent: "center",
-                    padding: 4,
                 }}
                 onPress={onPress}
                 activeOpacity={0.7}
             >
                 <View
                     style={{
-                        width: 50,
-                        height: 50,
+                        width: "100%",
+                        height: "100%",
                         alignItems: "center",
                         justifyContent: "center",
-                        backgroundColor: dateBackgroundColor,
-                        borderRadius: 10,
-                        ...(isActive && {
-                            boxShadow: [
-                                {
-                                    offsetX: 0,
-                                    offsetY: 0,
-                                    blurRadius: 6,
-                                    spreadDistance: 3,
-                                    color: adjustLightness(dateBackgroundColor, 30),
-                                },
-                            ],
-                        }),
+                        backgroundColor: colors.secondary,
+                        borderRadius: 16,
+                        borderWidth: allTasksCompleted ? 1.5 : 0,
+                        borderColor: "#129e43ff",
                     }}
                 >
-                    <Text preset="label1">{contracted[1]}</Text>
-                    <Text preset="label2">{contracted[0]}</Text>
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            fontFamily: "SemiBold",
+                            color: isEvaluation ? "#ff4b4b" : colors.contrast,
+                        }}
+                    >
+                        {contracted[1]}
+                    </Text>
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            fontFamily: "SemiBold",
+                            color: isEvaluation ? "#ff4b4b" : colors.contrast,
+                            marginTop: -12,
+                        }}
+                    >
+                        {contracted[0]}
+                    </Text>
                 </View>
             </TouchableOpacity>
         );
@@ -77,8 +76,7 @@ export default function HomeworkDatesRow({
 
             const tasks = mergedHomeworks ? (mergedHomeworks[date] ?? []) : [];
             const allTasksCompleted =
-                meta?.allTasksCompleted ??
-                (tasks.length > 0 && tasks.every(({ isDone }) => isDone === "done"));
+                tasks.length > 0 && tasks.every(({ isDone }) => isDone === "done");
 
             return (
                 <HomeworkDateItem
@@ -97,15 +95,24 @@ export default function HomeworkDatesRow({
     if (!datesData || Object.keys(datesData).length === 0) return null;
 
     return (
-        <View style={[{ flexDirection: "row", paddingHorizontal: 9 }, style]}>
+        <View
+            style={[
+                {
+                    flexDirection: "row",
+                    paddingHorizontal: 9,
+                    alignContent: "flex-end",
+                },
+                style,
+            ]}
+        >
             <FlatList
                 data={Object.entries(datesData)}
                 horizontal
                 renderItem={renderDateItem}
                 contentContainerStyle={{
                     gap: 12,
-                    paddingVertical: 10,
                     paddingHorizontal: 6,
+                    alignItems: "flex-end",
                 }}
                 keyExtractor={([date]) => date}
                 showsHorizontalScrollIndicator={false}
@@ -113,3 +120,4 @@ export default function HomeworkDatesRow({
         </View>
     );
 }
+
