5cbb51f0c9ae71cc1eb0ba1fea424de69ef25b4e
As de Pique <as2pick.card@outlook.fr>
2026-09-01 00:11:51 +0200

    pre-create release notes page

--- src/screens/Client/Settings/ReleaseNotesSetting.jsx
+++ src/screens/Client/Settings/ReleaseNotesSetting.jsx
@@ -1,7 +1,23 @@
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import SettingSectionLayout from "./components/SettingSectionLayout";
 
 export default function ReleaseNotesScreen({ route }) {
     const { label } = route.params;
 
-    return <SettingSectionLayout label={label}></SettingSectionLayout>;
+    return (
+        <SettingSectionLayout label={label}>
+            <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: "hsla(0, 0%, 100%, .24)",
+                        borderRadius: 32,
+                        marginBottom: 30,
+                        marginHorizontal: 10,
+                    }}
+                ></View>
+            </SafeAreaView>
+        </SettingSectionLayout>
+    );
 }
